91431833a7c5bfaf38f87ebdc2093bbbc7d01d5c
Duane Wandless <dwandless@pixmaclt10.local>
2011-02-06 08:33:40 -0500

    add SPEvents strings for AddEventListener

--- enum.cs
+++ enum.cs
@@ -3,8 +3,56 @@ namespace Sparrow
 {
 	public class SPEvents
 	{
+		/// <summary>
+		/// SP_EVENT_TYPE_TRIGGERED
+		/// </summary>
 		public const string ButtonTriggered = "triggered";
+		/// <summary>
+		/// SP_EVENT_TYPE_TOUCH
+		/// </summary>
 		public const string Touch = "touch";
+		/// <summary>
+		/// SP_EVENT_TYPE_MOVIE_COMPLETED
+		/// </summary>
+		public const string MovieCompleted = "movieCompleted";
+		/// <summary>
+		/// SP_EVENT_TYPE_ENTER_FRAME
+		/// </summary>
+		public const string EnterFrame = "enterFrame";
+		/// <summary>
+		/// SP_EVENT_TYPE_ADDED
+		/// </summary>
+		public const string Added = "added";
+		/// <summary>
+		/// SP_EVENT_TYPE_ADDED_TO_STAGE
+		/// </summary>
+		public const string AddedToStage = "addedToStage";
+		/// <summary>
+		/// SP_EVENT_TYPE_REMOVED
+		/// </summary>
+		public const string Remove = "removed";
+		/// <summary>
+		/// SP_EVENT_TYPE_REMOVED_FROM_STAGE
+		/// </summary>
+		public const string RemoveFromStage = "removedFromStage";
+		/// <summary>
+		/// SP_EVENT_TYPE_SOUND_COMPLETED
+		/// </summary>
+		public const string SoundCompleted = "soundCompleted";
+		/// <summary>
+		/// SP_EVENT_TYPE_TWEEN_STARTED
+		/// </summary>
+		public const string TweenStarted = "tweenStarted";
+		/// <summary>
+		/// SP_EVENT_TYPE_TWEEN_UPDATED
+		/// </summary>
+		public const string TweenUpdated = "tweenUpdated";
+		/// <summary>
+		/// SP_EVENT_TYPE_TWEEN_COMPLETED
+		/// </summary>
+		public const string TweenCompleted = "tweenCompleted";
+	
+		
 	}
 	
 	public class  SPAudioSessionCategory
